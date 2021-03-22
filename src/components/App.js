b72cb9fc883a3776ff0850541d877ea299a48ba2
navigator515 <gksrlf515@hanmail.net>
2021-03-22 16:37:07 +0900

    Auth add Project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import {authService} from "fBase";
+
+function App() {
+  
+const [isLoggedIn,setIsLoggedIn] = useState(authService.currentUser);
+console.log(authService.currentUser);
+  return( 
+    <>
+   <AppRouter isLoggedIn={isLoggedIn}/>
+   <footer>&copy; {new Date().getFullYear()} Kwitter</footer>
+   </>
+    )
+}
+
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Home =()=>{
+    <span>Home</span>
+    return(
+        <div>Home</div>
+    );
+}
+export default Home;
