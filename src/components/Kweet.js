a1f19d88687426806de08de8b347494c4c3c0de5
navigator515 <gksrlf515@hanmail.net>
2021-03-23 14:04:13 +0900

    delete 기능 추가

--- src/components/Kweet.js
+++ src/components/Kweet.js
@@ -1,17 +1,28 @@
+import { dbService } from "fBase";
 import React from "react";
 
-const Kweet=({kweetObj, isOwner})=>(
+const Kweet=({kweetObj, isOwner})=>{
+      const onDeleteClick = async () =>{
+          const ok = window.confirm("Are you sure you want to delte this kweet?");
+          if(ok){
+            await dbService.doc(`kweets/${kweetObj.id}`).delete();
+          }else{
+
+          }
+
+      }
+    return(
    
         <div>
             <h4>{kweetObj.text}</h4>
             {isOwner && (
             <>
-            <button>Delete Kweet</button>
+            <button onClick={onDeleteClick}>Delete Kweet</button>
             <button>Edit Kweet</button>
             </>
             )}
         </div>
             
-);
+)};
 
 export default Kweet;
