e550d17ab413a887ee0fb847fa6fcdd3f3eab93e
navigator515 <gksrlf515@hanmail.net>
2021-03-23 15:34:29 +0900

    update 기능 추가

--- src/components/Kweet.js
+++ src/components/Kweet.js
@@ -1,7 +1,9 @@
 import { dbService } from "fBase";
-import React from "react";
+import React, { useState } from "react";
 
 const Kweet=({kweetObj, isOwner})=>{
+    const [editing, setEditing]= useState(false);
+    const [newKweet, setNerKweet]=useState(kweetObj.text);
       const onDeleteClick = async () =>{
           const ok = window.confirm("Are you sure you want to delte this kweet?");
           if(ok){
@@ -11,18 +13,49 @@ const Kweet=({kweetObj, isOwner})=>{
           }
 
       }
+
+      const toggleEditing = () => setEditing((prev)=>!prev);
+      const onSubmit=async (event)=>{
+          event.preventDefault();
+          console.log(kweetObj, newKweet);
+        await dbService.doc(`kweets/${kweetObj.id}`).update({
+            text:newKweet,
+        });
+        setEditing(false);//에디팅 모드를 끝낸다 
+      }
+      const onChange=(event)=>{
+          const {target:{value},
+        }=event;
+        setNerKweet(value);
+      }
+
     return(
    
         <div>
+           {editing ? (
+               <>
+               <form onSubmit={onSubmit}>
+                   <input type="text" value={newKweet}
+                    placeholder="Edit your kweet" 
+                   required
+                   onChange={onChange}/>
+                   <input type="submit" value="Update Kweet"/>
+               </form>
+               <button onClick={toggleEditing}>Cancel</button>
+               </>
+           ):(
+               <>
             <h4>{kweetObj.text}</h4>
             {isOwner && (
             <>
             <button onClick={onDeleteClick}>Delete Kweet</button>
-            <button>Edit Kweet</button>
+            <button onClick={toggleEditing}>Edit Kweet</button>
             </>
             )}
+           </>
+           )}
         </div>
             
-)};
+    )};
 
 export default Kweet;
