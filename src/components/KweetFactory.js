51bf360535a7f0ceb76bcadab8cbed4ae052982f
navigator515 <gksrlf515@hanmail.net>
2021-04-01 23:16:07 +0900

    주석 추가 및 라인 정리

--- src/components/KweetFactory.js
+++ src/components/KweetFactory.js
@@ -8,28 +8,38 @@ const KweetFactory = ({userObj}) =>{
     const [kweet,setKweet]=useState("");
     const [attachment, setAttachment]= useState("");
 
+    //제출 시 동작 함수
     const onSubmit= async (event)=>{
-        if (kweet === "") {
+        if (kweet === "") // 아무것도 입력 안했을 시에 그냥 return 
+         {
             return;
           }
 
-        event.preventDefault();
-        let attachmentUrl="";
-       if(attachment!==""){
+        event.preventDefault();//contorll 가져오고
+        let attachmentUrl="";    
+       if(attachment!=="")//사진이 있다면
+       {
+         //사진을 fireStorage 에 올리고 url 을 다운 받는다  
         const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
         const response= await attachmentRef.putString(attachment, "data_url");
         attachmentUrl= await response.ref.getDownloadURL();
        };
+       //트윗 객체 생성
        const kweetObj = {
             text:kweet,
             createdAt:Date.now(),
             creatorId:userObj.uid,
             attachmentUrl,
         }
+        //fire DB 에 트윗 객체 저장
         await dbService.collection("kweets").add(kweetObj);
+        // 저장 후에는 다시 초기화 시켜 준다.
         setKweet("");
         setAttachment("");
     }
+    //입력을 해서 변화가 생겼을 때 
+    //입력 창이 하나 이므로 그냥 한개의 event 만 받아서 target 에 넣어주고 setKweet 해준다 
+    //결국 입력 값을 보여주는 함수
     const onChange=(event)=>{
         const {target:{value},
     }=event;
@@ -96,7 +106,7 @@ return (
                         placeholder="What's on your mind?"
                         maxLength={200}
                         />
-
+ {/* //사진 첨부 기능은 넣지만 보여주지는 않는다  */}
                     <input
                         id="attach-file"
                         type="file"
@@ -119,7 +129,7 @@ return (
      </div>
 
  </div>
-
+//이상 이미지 처부한 경우 
 :
     <div>
         {attachment && (
@@ -155,6 +165,7 @@ return (
         <span>Add photos</span>
         <FontAwesomeIcon icon={faPlus} />
       </label>
+      {/* //사진 첨부 기능은 넣지만 보여주지는 않는다  */}
       <input
         id="attach-file"
         type="file"
